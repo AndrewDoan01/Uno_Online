2aeb3e2d6dbb46cc6fa4bd1589069e08b4b74cfb
HiQAnn <23520006@gm.uit.edu.vn>
2024-10-28 09:14:50 +0700

    UI Update

---
+++ UnoGameOnline/Assets/Script/Card.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public Image cardImage; // Tham chiếu đến thành phần Image
+
+    void Awake()
+    {
+        cardImage = GetComponentInChildren<Image>(); // Lấy thành phần Image từ con
+    }
+
+    public void Setup(Sprite newSprite)
+    {
+        cardImage.sprite = newSprite; // Thiết lập hình ảnh cho thẻ
+    }
+}
