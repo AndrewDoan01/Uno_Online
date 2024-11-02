837522d57ced0b374b9e7931880f6e9822d19658
HiQAnn <23520006@gm.uit.edu.vn>
2024-11-02 21:27:17 +0700

    Asset Update
    1. Update Script
    2. Remove duplicated files

---
+++ UnoGameOnline/Assets/Script/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Button drawCardButton; // Nút rút thẻ
+    public Button playCardButton; // Nút chơi thẻ
+    public Button skipTurnButton; // Nút bỏ lượt
+
+    private CardDisplayManagement cardDisplayManager; // Tham chiếu đến CardDisplayManager
+
+    void Start()
+    {
+        // Lấy tham chiếu đến CardDisplayManager
+        cardDisplayManager = FindObjectOfType<CardDisplayManagement>();
+
+        // Gán các hàm xử lý cho các nút
+        playCardButton.onClick.AddListener(PlayCard);
+        skipTurnButton.onClick.AddListener(SkipTurn);
+    }
+
+    void DrawCard()
+    {
+        // Logic để rút thẻ
+        Debug.Log("Rút thẻ!");
+        // Ví dụ: cardDisplayManager.DrawCard();
+    }
+
+    void PlayCard()
+    {
+        // Logic để chơi thẻ
+        Debug.Log("Chơi thẻ!");
+        // Ví dụ: cardDisplayManager.PlayCard();
+    }
+
+    void SkipTurn()
+    {
+        // Logic để bỏ lượt
+        Debug.Log("Bỏ lượt!");
+        // Ví dụ: Chuyển lượt cho người chơi tiếp theo
+    }
+}
