294295525d29d9452ca79b5359206d3a71c4dab5
Lê Đặng Quốc An <160481299+LeDangQuocAn@users.noreply.github.com>
2024-11-21 21:57:32 +0700

    Create Player.cs

---
+++ UnoOnline/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoOnline
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public List<Card> Hand { get; set; }
+
+        public bool IsTurn { get; set; }
+
+
+        public Player() // Constructor mặc định 
+        {
+            Hand = new List<Card>();
+        }
+
+        public Player(string name)
+        {
+            Name = name;
+            Hand = new List<Card>();
+            IsTurn = false;
+        }
+    }
+}
