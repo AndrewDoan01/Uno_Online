68b3c3c841895b421d6a63fc0477852f0750b89b
AndrewDoan01 <23520006@gm.uit.edu.vn>
2024-11-22 13:46:09 +0700

    PlayerUpdate

--- UnoOnline/Player.cs
+++ UnoOnline/Player.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace UnoOnline
+public class Player
 {
-    internal class Player
+    public string Name { get; set; }
+    public List<Card> Hand { get; set; }
+
+    public bool IsTurn { get; set; }
+
+
+    public Player() // Constructor mặc định 
     {
+        Hand = new List<Card>();
+    }
+
+    public Player(string name)
+    {
+        Name = name;
+        Hand = new List<Card>();
+        IsTurn = false;
     }
 }
