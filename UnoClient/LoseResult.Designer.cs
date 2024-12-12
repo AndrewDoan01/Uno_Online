b50fc7e4c7471cdf2a1f0efda04b99f588060103
Lê Đặng Quốc An <160481299+LeDangQuocAn@users.noreply.github.com>
2024-12-13 01:21:50 +0700

    Win & Lose Result

--- UnoClient/LoseResult.Designer.cs
+++ UnoClient/LoseResult.Designer.cs
@@ -28,37 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoseResult));
             this.btnFinish = new System.Windows.Forms.Button();
             this.btnRestart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnFinish
             // 
-            this.btnFinish.Location = new System.Drawing.Point(536, 198);
+            this.btnFinish.BackColor = System.Drawing.Color.Transparent;
+            this.btnFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinish.Location = new System.Drawing.Point(49, 455);
             this.btnFinish.Name = "btnFinish";
             this.btnFinish.Size = new System.Drawing.Size(115, 54);
             this.btnFinish.TabIndex = 3;
             this.btnFinish.Text = "Finish";
-            this.btnFinish.UseVisualStyleBackColor = true;
-            this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click);
+            this.btnFinish.UseVisualStyleBackColor = false;
             // 
             // btnRestart
             // 
-            this.btnRestart.Location = new System.Drawing.Point(149, 198);
+            this.btnRestart.BackColor = System.Drawing.Color.Transparent;
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.Location = new System.Drawing.Point(431, 456);
             this.btnRestart.Name = "btnRestart";
             this.btnRestart.Size = new System.Drawing.Size(115, 54);
             this.btnRestart.TabIndex = 2;
             this.btnRestart.Text = "Restart";
-            this.btnRestart.UseVisualStyleBackColor = true;
-            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            this.btnRestart.UseVisualStyleBackColor = false;
             // 
             // LoseResult
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(596, 583);
             this.Controls.Add(this.btnFinish);
             this.Controls.Add(this.btnRestart);
+            this.DoubleBuffered = true;
             this.Name = "LoseResult";
             this.Text = "LoseResult";
             this.ResumeLayout(false);
--- UnoClient/WinResult.Designer.cs
+++ UnoClient/WinResult.Designer.cs
@@ -28,37 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinResult));
             this.btnRestart = new System.Windows.Forms.Button();
             this.btnFinish = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnRestart
             // 
-            this.btnRestart.Location = new System.Drawing.Point(133, 265);
+            this.btnRestart.BackColor = System.Drawing.Color.Transparent;
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.Location = new System.Drawing.Point(436, 457);
             this.btnRestart.Name = "btnRestart";
             this.btnRestart.Size = new System.Drawing.Size(115, 54);
             this.btnRestart.TabIndex = 0;
             this.btnRestart.Text = "Restart";
-            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.UseVisualStyleBackColor = false;
             this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
             // 
             // btnFinish
             // 
-            this.btnFinish.Location = new System.Drawing.Point(520, 265);
+            this.btnFinish.BackColor = System.Drawing.Color.Transparent;
+            this.btnFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinish.Location = new System.Drawing.Point(46, 455);
             this.btnFinish.Name = "btnFinish";
             this.btnFinish.Size = new System.Drawing.Size(115, 54);
             this.btnFinish.TabIndex = 1;
             this.btnFinish.Text = "Finish";
-            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.UseVisualStyleBackColor = false;
             this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click);
             // 
             // WinResult
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(596, 583);
             this.Controls.Add(this.btnFinish);
             this.Controls.Add(this.btnRestart);
+            this.DoubleBuffered = true;
             this.Name = "WinResult";
             this.Text = "WinResult";
             this.ResumeLayout(false);
