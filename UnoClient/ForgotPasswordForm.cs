63c6f41daa5e1adbcc0d2d8a1d81284f209ac50c
huyhcmuit <huyadchemistry123@gmail.com>
2024-12-15 19:33:28 +0700

    Database update

---
+++ UnoClient/ForgotPasswordForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameApp
+{
+    public partial class ForgotPasswordForm : Form
+    {
+        public ForgotPasswordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void retrievePasswordButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text; 
+            string email = emailTextBox.Text;       
+
+           
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["GameDB"].ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    
+                    string query = "SELECT Email FROM Users WHERE Username = @Username";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        string storedEmail = (string)cmd.ExecuteScalar(); // Lấy email từ cơ sở dữ liệu
+
+                        if (storedEmail != null && storedEmail.Equals(email, StringComparison.OrdinalIgnoreCase))
+                        {
+                            // Tạo mật khẩu mới
+                            string newPassword = GenerateRandomPassword();
+
+                            // Băm mật khẩu mới
+                            string hashedPassword = PasswordHelper.HashPassword(newPassword);
+
+                            // Cập nhật mật khẩu mới vào cơ sở dữ liệu
+                            UpdatePasswordInDatabase(username, hashedPassword);
+
+                            // Hiển thị mật khẩu mới trên giao diện
+                            passwordTextBox.Text = newPassword; // Gán mật khẩu mới vào TextBox
+                        }
+                        else
+                        {
+                            passwordTextBox.Text = "Thông tin không hợp lệ."; // Thông báo lỗi nếu không khớp email
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    passwordTextBox.Text = $"Lỗi: {ex.Message}"; // Thông báo lỗi khi kết nối hoặc truy vấn
+                }
+            }
+        }
+
+        // Hàm tạo mật khẩu ngẫu nhiên
+        private string GenerateRandomPassword()
+        {
+            return Guid.NewGuid().ToString("N").Substring(0, 8); // Tạo mật khẩu ngẫu nhiên dài 8 ký tự
+        }
+
+        // Hàm cập nhật mật khẩu mới vào cơ sở dữ liệu
+        private void UpdatePasswordInDatabase(string username, string hashedPassword)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["GameDB"].ConnectionString))
+            {
+                conn.Open();
+                string query = "UPDATE Users SET PasswordHash = @PasswordHash WHERE Username = @Username";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@PasswordHash", hashedPassword);
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ UnoClient/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace GameApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(134, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 56);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Đăng ký ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button2.Location = new System.Drawing.Point(460, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 56);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Đăng nhập";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
